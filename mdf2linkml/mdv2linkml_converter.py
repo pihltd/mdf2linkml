031f732d94825091f9d27d6f278113dba3ed4821
Todd Pihl <todd.pihl@gmail.com>
2022-04-11 16:07:08 -0400

    Updated parsing and added output

--- mdf2linkml/mdv2linkml_converter.py
+++ mdf2linkml/mdv2linkml_converter.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python
 
 import argparse
-from ctypes.wintypes import PPOINT
 import yaml
 import pprint
 
@@ -9,7 +8,14 @@ def load_mdf_file(mdf_file_name):
     #Take a file text name, convert the yaml to a python object and return the python object
     with open(mdf_file_name,'r') as file:
         yamlobject = yaml.safe_load(file)
+        #yamlobject = yaml.load(file)
     return yamlobject
+
+def write_linkml_file(modeljson, filename):
+    outfile = open(filename,'w')
+    yaml.safe_dump(modeljson, outfile)
+    outfile.close()
+
 
 def parse_model_nodes(yamlobject):
     #Parses out all of the MDF nodes
@@ -22,16 +28,50 @@ def parse_model_nodes(yamlobject):
             pprint.pprint(nodearray[mdfnode][mdfkey])
         #pprint.pprint(nodearray[mdfnode])
 
+def parse_classes(nodeobject):
+    #MDF (well, ICDC at least), uses Tags -> Categories to organize data elements.  Those are likely candidates for LinkmlClasses
+    classlist = []
+    for node in nodeobject.keys():
+        classlist.append(nodeobject[node]['Tags']['Category'])
+    return classlist
+
+def buildLinkml(linkmljson, nodename, description, isathing, propertylist):
+    """LinkML JSON structure
+    {'classes': {classname: {attributes:[attributenames]}}}
+    """
+    #Step one, create the attributes list
+    attributedict = {'attributes': propertylist}
+    classjson = linkmljson['classes']
+    classjson[nodename] = attributedict
+    classjson[nodename]['description'] = description
+    classjson[nodename]['is_a'] = isathing
+    linkmljson['classes'] = classjson
+    return linkmljson
+
+
+
 def main(args):
     modelobject = load_mdf_file(args.modelfile)
-    propsobject = load_mdf_file(args.propsfile)
-    modeldict = modelobject['Nodes']
-    propsdict = propsobject['PropDefinitions']
-    for key in modeldict.keys():
-        for property in modeldict[key]['Props']:
-            print(key)
-            print(property)
-            pprint.pprint(propsdict[property]['Desc'])
+    #propsobject = load_mdf_file(args.propsfile)
+    modelnodedict = modelobject['Nodes']
+    #propertyobject = propsobject['PropDefinitions']
+    linkmljson = {}
+    linkmljson['classes'] = {}
+    for nodename in modelnodedict.keys():
+        isathing = modelnodedict[nodename]['Tags']['Category']
+        description = modelnodedict[nodename]['Desc']
+        propertylist = modelnodedict[nodename]['Props']
+        linkmljson = buildLinkml(linkmljson, nodename, description, isathing, propertylist)
+    #pprint.pprint(linkmljson)
+    write_linkml_file(linkmljson, args.outputfile)
+
+
+
+    #for key in modeldict.keys():
+    #    for property in modeldict[key]['Props']:
+    #        print(key)
+    #        print(property)
+    #        pprint.pprint(propsdict[property]['Desc'])
     #print(type(modelkeys))
     #pprint.pprint(yamlobject)
     #parse_model_nodes(modelobject)
@@ -39,7 +79,8 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--modelfile", required = True, help="MDF model file")
-    parser.add_argument("-p", "--propsfile", required = True, help = "Output file")
+    parser.add_argument("-p", "--propsfile", required = True, help = "MDF Properties file")
+    parser.add_argument("-o", "--outputfile", required = True, help = "LinkML YAML file")
 
     args = parser.parse_args()
     main(args)
