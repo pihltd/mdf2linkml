344fa2a17ef03e170001e55e3d4c041a224321c1
Todd Pihl <todd.pihl@gmail.com>
2022-04-12 16:17:46 -0400

    Updated prop annotation

--- mdf2linkml/mdv2linkml_converter.py
+++ mdf2linkml/mdv2linkml_converter.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 
 import argparse
+from attr import attributes
 import yaml
 import pprint
 
@@ -8,7 +9,6 @@ def load_mdf_file(mdf_file_name):
     #Take a file text name, convert the yaml to a python object and return the python object
     with open(mdf_file_name,'r') as file:
         yamlobject = yaml.safe_load(file)
-        #yamlobject = yaml.load(file)
     return yamlobject
 
 def write_linkml_file(modeljson, filename):
@@ -39,42 +39,80 @@ def buildLinkml(linkmljson, nodename, description, isathing, propertylist):
     """LinkML JSON structure
     {'classes': {classname: {attributes:[attributenames]}}}
     """
-    #Step one, create the attributes list
+    #Create the attributes dictionary
     attributedict = {'attributes': propertylist}
+    #Get the class json object for the key 'classes'.  This should get all classes/nodes
     classjson = linkmljson['classes']
+    #Assign the attribute dictionary to the class json with the node as the key.  Currently doesn't check if the key has been assigned before
     classjson[nodename] = attributedict
+    #Tack on description, is_a, required
     classjson[nodename]['description'] = description
     classjson[nodename]['is_a'] = isathing
     linkmljson['classes'] = classjson
     return linkmljson
 
+def updateFieldname(name):
+    corrections = {'Desc':'description', 'Req':'required'}
+    if name in corrections:
+        name = corrections[name]
+    return name
+
+def updateValues(name):
+    corrections = {'Yes': 'true', 'No' :'false', 'Preferred': 'true'}
+    if name in corrections:
+        name = corrections[name]
+    return name
+
+def propertyLookup(propertydict, propertylist, fieldlist):
+    #Create the list that evenetually gets returned
+    updateddictionary = []
+    #For each attribute in the property list, see if the attribute is in the dictionary
+    if propertylist is not None:
+        for property in propertylist:
+            newvalue = None
+            if property in propertydict:
+                tempdict = {}
+                for field in fieldlist:  #Desc, Req
+                    if field in propertydict[property]:
+                        newvalue = propertydict[property][field]
+                        newvalue = updateValues(newvalue)
+                        field = updateFieldname(field)
+                        tempdict[field] =newvalue
+                if tempdict:  #tempdict is NOT empty
+                    finaltemp = {}
+                    finaltemp[property] = tempdict
+                    updateddictionary.append(finaltemp)
+                else:
+                    updateddictionary.append(property)
+            else:
+                updateddictionary.append(property)
+    return updateddictionary
+
+
+
+
+
 
 
 def main(args):
     modelobject = load_mdf_file(args.modelfile)
-    #propsobject = load_mdf_file(args.propsfile)
-    modelnodedict = modelobject['Nodes']
-    #propertyobject = propsobject['PropDefinitions']
+    propsobject = load_mdf_file(args.propsfile)
+    nodedictionary = modelobject['Nodes']
+    propertydict = propsobject['PropDefinitions']
     linkmljson = {}
     linkmljson['classes'] = {}
-    for nodename in modelnodedict.keys():
-        isathing = modelnodedict[nodename]['Tags']['Category']
-        description = modelnodedict[nodename]['Desc']
-        propertylist = modelnodedict[nodename]['Props']
-        linkmljson = buildLinkml(linkmljson, nodename, description, isathing, propertylist)
-    #pprint.pprint(linkmljson)
+
+    for nodename in nodedictionary.keys():
+        isathing = nodedictionary[nodename]['Tags']['Category']
+        nodeDescription = nodedictionary[nodename]['Desc']
+        propertylist = nodedictionary[nodename]['Props']
+        #Need to provide a dictionary of MDF attribues to look for and their LinkML counterparts
+        fieldlist = {'Req' : 'request', 'Desc' : 'description'}
+        propertylist = propertyLookup(propertydict, propertylist,fieldlist)
+        linkmljson = buildLinkml(linkmljson, nodename, nodeDescription, isathing, propertylist)
+
     write_linkml_file(linkmljson, args.outputfile)
 
-
-
-    #for key in modeldict.keys():
-    #    for property in modeldict[key]['Props']:
-    #        print(key)
-    #        print(property)
-    #        pprint.pprint(propsdict[property]['Desc'])
-    #print(type(modelkeys))
-    #pprint.pprint(yamlobject)
-    #parse_model_nodes(modelobject)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
